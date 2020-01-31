0fbcb540a8e71e1be2366e0c4a841059795c7b50
shamimfahad <shamimfahadshuvo@gmail.com>
2020-01-31 23:14:00 +0600

    Updates

---
+++ precache-manifest.1df346ebb2c63b6ad2c995996bafae1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8a3ed3544447392546380a8691d16c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "94d0462ffaa01a2bf9b3",
+    "url": "/monsters/static/css/main.e61176e0.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94d0462ffaa01a2bf9b3",
+    "url": "/monsters/static/js/main.7ad74f04.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
